2479921bcc6262c0317117dc7d66abce8136016f
Zeynschweyk <Zschweyk@gmail.com>
2022-02-05 18:28:16 -0800

    Yes! Got it to work! It generated the correct shortest path!

--- main.py
+++ main.py
@@ -19,31 +19,31 @@ class Graph:
     def get_vertices(self):
         return tuple(self.table.keys())
 
-def shortest_path(graph: Graph, start, finish):
+def shortest_path(graph: Graph, start_vertex, end_vertex):
     table = {}
     for vertex in graph.get_vertices():
-        table.update({vertex: [0 if vertex == start else float("inf"), ""]})
+        table.update({vertex: [0 if vertex == start_vertex else float("inf"), ""]})
     print(table)
+
     processed = []
-    vertex_to_process = min(dict(filter(lambda item: item[0] not in processed, table.items())),
+    calculate_vertex_to_process = lambda: min(dict(filter(lambda item: item[0] not in processed, table.items())),
                          key=lambda key: table[key][0])
+    vertex_to_process = calculate_vertex_to_process()
     print(vertex_to_process)
-    # table = {}
-    # for row, vertex in enumerate(graph.get_vertices()):
-    #     table.update({row: [vertex, 0 if vertex == start else float("inf"), ""]})
-    # print(table)
-    # processed = []
-    # # Find the row to process
-    # row_to_process = min(dict(filter(lambda item: item[1][0] not in processed, table.items())), key=lambda key: table[key][1])
-    # print(row_to_process)
-    #
-    # while table[row_to_process][0] != finish:
-    #     branches = {vertex: cost for vertex, cost in graph.get_branches_from(table[row_to_process][0]).items() if vertex not in processed}
-    #     print(branches)
-    #     processed.append(table[row_to_process][0])
-    #     row_to_process = min(dict(filter(lambda item: item[1][0] not in processed, table.items())),
-    #                          key=lambda key: table[key][1])
-    # print("Done")
+
+    while vertex_to_process != end_vertex:
+        branches = {vertex: cost for vertex, cost in graph.get_branches_from(vertex_to_process).items() if
+                    vertex not in processed}
+        print(branches)
+        for vertex, cost in branches.items():
+            dist_from_start_vertex = table[vertex_to_process][0] + cost
+            if dist_from_start_vertex < table[vertex][0]:
+                table[vertex] = [dist_from_start_vertex, vertex_to_process]
+
+        processed.append(vertex_to_process)
+        vertex_to_process = calculate_vertex_to_process()
+
+    return table
 
 
 
@@ -60,7 +60,7 @@ table = {
 }
 
 g = Graph(table)
-shortest_path(g, "A", "G")
+print(shortest_path(g, "A", "G"))
 
 
 
