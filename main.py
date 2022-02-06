63e3dc0a6d94d08162afee23b8bd630e2b648a09
Zeynschweyk <Zschweyk@gmail.com>
2022-02-05 18:07:51 -0800

    Updated min and filter, and the table, to grab the vertex to process, instead of the row to process, since it will make it easier.

--- main.py
+++ main.py
@@ -21,21 +21,29 @@ class Graph:
 
 def shortest_path(graph: Graph, start, finish):
     table = {}
-    for row, vertex in enumerate(graph.get_vertices()):
-        table.update({row: [vertex, 0 if vertex == start else float("inf"), ""]})
+    for vertex in graph.get_vertices():
+        table.update({vertex: [0 if vertex == start else float("inf"), ""]})
     print(table)
     processed = []
-    # Find the row to process
-    row_to_process = min(dict(filter(lambda item: item[1][0] not in processed, table.items())), key=lambda key: table[key][1])
-    print(row_to_process)
-
-    while table[row_to_process][0] != finish:
-        branches = {vertex: cost for vertex, cost in graph.get_branches_from(table[row_to_process][0]).items() if vertex not in processed}
-        print(branches)
-        processed.append(table[row_to_process][0])
-        row_to_process = min(dict(filter(lambda item: item[1][0] not in processed, table.items())),
-                             key=lambda key: table[key][1])
-    print("Done")
+    vertex_to_process = min(dict(filter(lambda item: item[0] not in processed, table.items())),
+                         key=lambda key: table[key][0])
+    print(vertex_to_process)
+    # table = {}
+    # for row, vertex in enumerate(graph.get_vertices()):
+    #     table.update({row: [vertex, 0 if vertex == start else float("inf"), ""]})
+    # print(table)
+    # processed = []
+    # # Find the row to process
+    # row_to_process = min(dict(filter(lambda item: item[1][0] not in processed, table.items())), key=lambda key: table[key][1])
+    # print(row_to_process)
+    #
+    # while table[row_to_process][0] != finish:
+    #     branches = {vertex: cost for vertex, cost in graph.get_branches_from(table[row_to_process][0]).items() if vertex not in processed}
+    #     print(branches)
+    #     processed.append(table[row_to_process][0])
+    #     row_to_process = min(dict(filter(lambda item: item[1][0] not in processed, table.items())),
+    #                          key=lambda key: table[key][1])
+    # print("Done")
 
 
 
