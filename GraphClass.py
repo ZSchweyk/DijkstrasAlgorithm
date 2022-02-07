fefbaf6bc2a8c8c69dc3999b6d70badd33de1cea
ZSchweyk <zeynschweyk@dpengineering.org>
2022-02-07 11:40:59 -0800

    Added a longest path method. Not completely sure how that would be useful.

--- GraphClass.py
+++ GraphClass.py
@@ -93,3 +93,79 @@ class Graph:
 
         # Return a tuple of the reversed version of path and the total cost of that shortest path.
         return path[::-1], table[end_vertex][0]
+
+    def longest_path(self, start_vertex, end_vertex):
+        """Returns a tuple containing the shortest path and total cost
+        between start_vertex and end_vertex of a given table. See GraphVisualization.jpg and the table below
+        to format the table properly to create a Graph object out of it."""
+
+        # Construct a dictionary, with the keys representing the vertices and the values representing a list
+        # containing the shortest distance from start_vertex and the previous vertex.
+        #
+        # Vertex | Shortest Distance from start_vertex | Previous Vertex
+        # A      | 0                                   |
+        # B      | infinity                            | A
+        # C      | infinity                            | A
+        # ...
+        table = {}
+        for vertex in self.get_vertices():
+            # Assume that every vertex is infinitely far away in the negative direction
+            table.update({vertex: [0 if vertex == start_vertex else float("-inf"), ""]})
+
+        processed = []  # Create a list to store all future processed vertices
+        # A function that calculates the next vertex to process. First, it filters out every vertex that has been
+        # processed, and then grabs the vertex with the shortest distance from start_vertex.
+        calculate_vertex_to_process = lambda: max(dict(filter(lambda item: item[0] not in processed, table.items())),
+                                                  key=lambda key: table[key][0])
+        # Store the result of the function above in vertex_to_process
+        vertex_to_process = calculate_vertex_to_process()
+
+        # Start looping until the destination vertex is about to be processed
+        while vertex_to_process != end_vertex:
+            # print("Vertex to process =", vertex_to_process)
+            # Grab all the branches and costs that vertex_to_process has, filtering out all the vertices that have been
+            # processed.
+            branches = {vertex: cost for vertex, cost in self.get_branches_from(vertex_to_process).items() if
+                        vertex not in processed}
+
+            # Deal with node islands; raise an exception if start_vertex and end_vertex are not connected.
+            # If the number of vertex_to_process's branches that haven't been processed is 0, then vertex_to_process is
+            # the last node to process on an island. Since everything else has already been processed, catch this event
+            # and raise an exception.
+            if len(branches) == 0:
+                raise Exception(f"\"{start_vertex}\" & \"{end_vertex}\" aren't connected. Node \"{vertex_to_process}\""
+                                f" can't be processed because it's the last on its island, which contains nodes"
+                                f" {[f'{node}' for node in processed] + [f'{vertex_to_process}']}"
+                                f" and were processed in that respective order.")
+
+            # Loop through each vertex and cost in the branches, and update the table accordingly.
+            for vertex, cost in branches.items():
+                # Calculate the distance from start_vertex to vertex_to_process
+                dist_from_start_vertex = table[vertex_to_process][0] + cost
+                if dist_from_start_vertex > table[vertex][0]:
+                    # If that calculated distance is < the distance that's already in the table, updated it so that that
+                    # value is the shortest distance from start_vertex to vertex_to_process
+                    table[vertex] = [dist_from_start_vertex, vertex_to_process]
+
+            # Append vertex_to_process to processed, which was defined above.
+            processed.append(vertex_to_process)
+            # Recalculate the next vertex to process, and update vertex_to_process to equal that new vertex for the next
+            # loop iteration.
+            vertex_to_process = calculate_vertex_to_process()
+
+        # Create a list that will hold the shortest path from start_vertex to end_vertex.
+        path = []
+        # Create a variable to store a changing vertex. Personally, I didn't want to change the value of end_vertex.
+        looping_vertex = end_vertex
+        # Append end_vertex to the path; the while loop below will fill in the rest.
+        path.append(looping_vertex)
+        # This while loop basically is exactly the same as repeated VLOOKUPs in Excel/Google Sheets.
+        while looping_vertex != start_vertex:
+            # Append the value obtained from looping_vertex serving as the key to the table dictionary.
+            path.append(table[looping_vertex][1])
+            # Update the value of looping_vertex.
+            looping_vertex = table[looping_vertex][1]
+
+
+        # Return a tuple of the reversed version of path and the total cost of that shortest path.
+        return path[::-1], table[end_vertex][0]
--- main.py
+++ main.py
@@ -13,51 +13,53 @@ g = Node("G")
 h = Node("H")
 i = Node("I")
 
-a.cost_to(b, 5)
-a.cost_to(c, 2)
-c.cost_to(a, 2)
-c.cost_to(b, 2)
-b.cost_to(a, 5)
-b.cost_to(c, 2)
-
-e.cost_to(d, 5)
-e.cost_to(f, 7)
-f.cost_to(e, 7)
-f.cost_to(d, 2)
-d.cost_to(e, 5)
-d.cost_to(f, 2)
-
-
-
-
-
+# Islands [a, b, c], [d, e, f], [g], [h], [i]
 # a.cost_to(b, 5)
 # a.cost_to(c, 2)
-#
-# b.cost_to(c, 2)
-# b.cost_to(e, 1)
-# b.cost_to(d, 4)
-# b.cost_to(a, 5)
-#
-# c.cost_to(e, 5)
-# c.cost_to(b, 2)
 # c.cost_to(a, 2)
+# c.cost_to(b, 2)
+# b.cost_to(a, 5)
+# b.cost_to(c, 2)
 #
+# e.cost_to(d, 5)
+# e.cost_to(f, 7)
+# f.cost_to(e, 7)
+# f.cost_to(d, 2)
 # d.cost_to(e, 5)
 # d.cost_to(f, 2)
-# d.cost_to(g, 3)
-# d.cost_to(b, 4)
-#
-# e.cost_to(f, 7)
-# e.cost_to(d, 5)
-# e.cost_to(b, 1)
-# e.cost_to(c, 5)
-#
-# f.cost_to(g, 1)
-# f.cost_to(d, 2)
-# f.cost_to(e, 7)
+
+
+
+
+
+a.cost_to(b, 5)
+a.cost_to(c, 2)
+
+b.cost_to(c, 2)
+b.cost_to(e, 1)
+b.cost_to(d, 4)
+b.cost_to(a, 5)
+
+c.cost_to(e, 5)
+c.cost_to(b, 2)
+c.cost_to(a, 2)
+
+d.cost_to(e, 5)
+d.cost_to(f, 2)
+d.cost_to(g, 3)
+d.cost_to(b, 4)
+
+e.cost_to(f, 7)
+e.cost_to(d, 5)
+e.cost_to(b, 1)
+e.cost_to(c, 5)
+
+f.cost_to(g, 1)
+f.cost_to(d, 2)
+f.cost_to(e, 7)
 
 
 graph = Graph([a, b, c, d, e, f, g, h, i])
-print(graph.shortest_path("A", "I"))
+print(graph.shortest_path("A", "G"))
+print(graph.longest_path("A", "G"))
 
