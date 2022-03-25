a013fbb0ffdcbf8ba59e2c78d41433d8b02a8abe
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-25 00:29:33 -0700

    added a ton of colleges and distances between them in terms of time. Can't find shortest path between something and itself.

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
 from NodeClass import Node
 from GraphClass import Graph
 
-a = Node("A")
-b = Node("B")
-c = Node("C")
-d = Node("D")
-e = Node("E")
-f = Node("F")
-g = Node("G")
+harvard = Node("Harvard")
+princeton = Node("Princeton")
+yale = Node("Yale")
+johns_hopkins = Node("Johns Hopkins")
+cornell = Node("Cornell")
+u_penn = Node("UPenn")
+brown = Node("Brown")
+university_of_rochester = Node("University of Rochester")
 
 Node.bi_directional = True
 
-a.cost_to(b, 5)
-a.cost_to(c, 2)
+harvard.cost_to(princeton, 4.567)
+harvard.cost_to(yale, 2.233)
+harvard.cost_to(johns_hopkins, 6.67)
+harvard.cost_to(cornell, 5.67)
+harvard.cost_to(u_penn, 5.383)
+harvard.cost_to(brown, .9167)
+harvard.cost_to(university_of_rochester, 6.2)
 
-b.cost_to(c, 2)
-b.cost_to(e, 1)
-b.cost_to(d, 4)
+princeton.cost_to(yale, 2.4)
+princeton.cost_to(johns_hopkins, 2.45)
+princeton.cost_to(cornell, 4.15)
+princeton.cost_to(u_penn, .783)
+princeton.cost_to(brown, 4.05)
+princeton.cost_to(university_of_rochester, 5.7667)
 
-c.cost_to(e, 5)
+yale.cost_to(johns_hopkins, 4.45)
+yale.cost_to(cornell, 4.85)
+yale.cost_to(u_penn, 2.8833)
+yale.cost_to(brown, 1.6833)
 
-d.cost_to(e, 5)
-d.cost_to(f, 2)
-d.cost_to(g, 3)
+johns_hopkins.cost_to(cornell, 5.15)
+johns_hopkins.cost_to(u_penn, 1.7)
+johns_hopkins.cost_to(brown, 6.1)
+johns_hopkins.cost_to(university_of_rochester, 5.71667)
 
-e.cost_to(f, 7)
+cornell.cost_to(u_penn, 3.91667)
+cornell.cost_to(brown, 5.5667)
+cornell.cost_to(university_of_rochester, 1.8)
 
-f.cost_to(g, 1)
+u_penn.cost_to(brown, 4.65)
+u_penn.cost_to(university_of_rochester, 5.5667)
+brown.cost_to(university_of_rochester, 6.2667)
 
-# Islands [a, b, c], [d, e, f], [g], [h], [i]
-# a.cost_to(b, 5)
-# a.cost_to(c, 2)
-# c.cost_to(a, 2)
-# c.cost_to(b, 2)
-# b.cost_to(a, 5)
-# b.cost_to(c, 2)
-#
-# e.cost_to(d, 5)
-# e.cost_to(f, 7)
-# f.cost_to(e, 7)
-# f.cost_to(d, 2)
-# d.cost_to(e, 5)
-# d.cost_to(f, 2)
-
-graph = Graph([a, b, c, d, e, f, g])
-print(graph.shortest_path("A", "G"))
+graph = Graph([harvard, princeton, yale, johns_hopkins, cornell, u_penn, brown])
+print(graph.shortest_path("University of Rochester", "University of Rochester"))
 
